729b094bed6308f253418fb32d9e7ebc442f2093
abigailelizabeth <abbyeb22@gmail.com>
2016-04-06 13:40:01 -0500

    huge commit, currently able to input departments, and grab it fron dropdown menus and store data

--- myapp.py
+++ myapp.py
@@ -1,12 +1,40 @@
-from flask import Flask
+import flask
+from flask import g
+app = flask.Flask(__name__)
 
-app = Flask(__name__)
+data=[]
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return flask.render_template('index.html')
+
+
+@app.route('/calculate')
+def calculate():
+    return flask.render_template('calculate.html')
+
+
+@app.route('/calculate', methods=['POST'])
+def render_data():
+
+    return flask.render_template('result.html')
+
+
+@app.route('/generate', methods=['POST'])
+def generate_grid():
+    size = int(flask.request.form['usersize'])
+    return flask.render_template('calculate.html', size=size)
+
+
+@app.route('/demo', methods=['POST'])
+def get_data():
+    for i in range(1, int(flask.request.form['size'])+1):
+        print('what about here')
+        data.append(flask.request.form.get('select'+ str(i)))
+    print(data)
+    return flask.redirect('/', code=301)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
